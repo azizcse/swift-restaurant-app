9114faaee491eba1b3bfd69a7c9b3ab0442007c0
Aziz <imazizul@gmail.com>
2021-04-10 00:27:46 +0600

    Initial Commit

---
+++ iDien/iDienApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDienApp.swift
+//  iDien
+//
+//  Created by Azizul Islam on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct iDienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
